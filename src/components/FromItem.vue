d06216a56025418cc72211a5a54eb698d5bab54e
dengsijia <2131242587@qq.com>
2021-05-12 10:58:07 +0800

    ******

--- src/components/FromItem.vue
+++ src/components/FromItem.vue
@@ -33,8 +33,11 @@
                 </div>
             </div>
         </div>
-        <div class="balance font-family-regular font-weight-4 text-left border-r border-l border-b">Balance：{{ stateFormat_(currCoin.balance,6) }}
-        </div>
+        <div class="balance flex flex-align-items-center font-family-regular font-weight-4 text-left border-r border-l border-b">Balance：
+			<div v-if="currCoin.balance" class="">{{ stateFormat_(currCoin.balance,6) }}</div>
+			<img class="loadingIcon" src="../../static/images/loading.gif" />
+		</div>
+		
         <h5 class="text-left" style="color: crimson" v-if="(currCoin.balance<currCoin.fromNum) && showerr">Current balance {{ stateFormat_(currCoin.balance,6) }} ,Insufficient amount</h5>
 		 <Tips :showTips="approveSuccessfullyTips"></Tips>
     </div>
@@ -123,6 +126,10 @@
 </script>
 
 <style scoped>
+	.loadingIcon{
+		width: 16px;
+		height: 16px;
+	}
     .fromInfo {
         height: 54px;
         padding: 10px 0;
